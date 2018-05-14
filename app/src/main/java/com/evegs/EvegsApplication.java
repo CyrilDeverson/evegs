71163f02cacea30a8b6f2c8dd75d5465b9d915bc
CyrilDeverson <cyril.deverson@free.fr>
2018-05-15 00:08:11 +0200

    Launch browser on startup

--- app/src/main/java/com/evegs/EvegsApplication.java
+++ app/src/main/java/com/evegs/EvegsApplication.java
@@ -1,12 +1,44 @@
 package com.evegs;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EvegsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvegsApplication.class, args);
+	}
+
+	@EventListener({ ApplicationReadyEvent.class })
+	void applicationReadyEvent() {
+		log.info("Application started ... launching browser now");
+		String url = "http://localhost:8080";
+		try {
+			openBrowser(new URI(url));
+		} catch (IOException | URISyntaxException e) {
+			log.warn("Failed to open browser. Please open your browser at this location: " + url);
+		}
+	}
+
+	private static void openBrowser(URI url) throws IOException, URISyntaxException {
+		if (Desktop.isDesktopSupported()) {
+			Desktop desktop = Desktop.getDesktop();
+			desktop.browse(url);
+		} else {
+			Runtime runtime = Runtime.getRuntime();
+			runtime.exec("xdg-open " + url);
+		}
 	}
 }
